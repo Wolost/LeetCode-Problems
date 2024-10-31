01b0998cb1ba6a167ccb30b3697ca142bd4d5959
Alejandro Muñoz <86081720+Wolost@users.noreply.github.com>
2024-10-31 10:01:54 -0500

    Fix LongestCommonPrefix problem

--- Python/longestCommonPrefix.py
+++ Python/longestCommonPrefix.py
@@ -3,21 +3,17 @@ from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        i = 0
-        count = len(strs)
-        prefx= [""]*count
-        if (count > 1):
-            aux = list(strs[0])
-            while i < count:
-                for char in strs[i]:
-                    if(char in aux):
-                        prefx[i] += char
-                    else:
-                        break
-                i += 1
-            return min(prefx)
-        else:    
-            return strs[0]
+        if not strs:
+            return ""
+        prefx = strs[0]
+        
+        for s in strs[1:]:
+            while not s.startswith(prefx):
+                prefx = prefx[:-1]
+                if not prefx:
+                    return ""
+
+        return prefx
 
 solution = Solution()
 
